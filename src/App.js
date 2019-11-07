817f9ba4645699f934a840f6ccacce0717bd4a12
Denis Gorshkov <satrasmatrassatras@gmail.com>
2019-11-07 22:57:55 +0300

    Up to 14 lesson
    Created app
    Maked base layout
    First component
    Divided on components
    CSS modules

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Profile from "./components/Content";
 
-function App() {
+// Импортировать CSS modules
+// - Header
+// - NavBar
+// - Profile
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='app-wrapper'>
+        <Header />
+        <NavBar />
+        <Profile />
     </div>
+
   );
-}
+};
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+
+const Title = () => {
+    return (
+        <h1> Header #1 </h1>
+    )
+};
+export default Title;
