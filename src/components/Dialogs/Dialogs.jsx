664186ce499612a41c8f766b38eff74f3f57fcfc
Denis Gorshkov <satrasmatrassatras@gmail.com>
2019-11-10 23:36:58 +0300

    Lesson 21
    Create base dialogs page markup

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,26 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div className={s.content}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Denis
+                </div>
+                <div className={s.dialog}>
+                    Olesya
+                </div>
+                <div className={s.dialog}>
+                    Margarita
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How you doin?</div>
+                <div className={s.message}>Good</div>
+            </div>
         </div>
     )
 }
