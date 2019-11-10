86136bd696eab53fd9824509ca0c5fe6f6f94cc9
Denis Gorshkov <satrasmatrassatras@gmail.com>
2019-11-10 23:53:35 +0300

    Update Dialogs.jsx
    Made NavLinks in Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
                 <div className={s.dialog + ' ' + s.active}>
-                    Denis
+                    <NavLink to='/dialogs/1'>Denis</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Olesya
+                    <NavLink to='/dialogs/2'>Denis</NavLink>
+
                 </div>
                 <div className={s.dialog}>
-                    Margarita
+                    <NavLink to='/dialogs/3'>Denis</NavLink>
+
                 </div>
                 <div className={s.dialog}>
-                    Andrey
+                    <NavLink to='/dialogs/4'>Denis</NavLink>
+
                 </div>
             </div>
             <div className={s.messages}>
