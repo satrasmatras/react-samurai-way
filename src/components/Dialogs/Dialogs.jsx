6b994d1b42f0fffca27c138326b55dd0371169ff
Denis Gorshkov <satrasmatrassatras@gmail.com>
2019-11-12 01:44:09 +0300

    Lesson 24. BLL
    Added some data to render. Map prelude...

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -18,20 +18,43 @@ const Message = (props) => {
 
 
 const Dialogs = (props) => {
+
+    // BLL
+    // UI
+
+    let dialogsData = [
+        {id: 1, name: 'Denis'},
+        {id: 2, name: 'Alex'},
+        {id: 3, name: 'Egor'},
+        {id: 4, name: 'Pasha'},
+        {id: 5, name: 'Saris'}
+    ];
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'I am OK'}
+    ];
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
-
-                <DialogItem name='Denis' id='1'/>
-                <DialogItem name='Viktoria' id='2'/>
-                <DialogItem name='Egor' id='3'/>
-                <DialogItem name='Pavel' id='4'/>
+                {
+                    dialogsData.map( dialogItem => <DialogItem name={dialogItem.name} id={dialogItem.id}/>)
+                }
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                {/*<DialogItem name='Viktoria' id='2'/>*/}
+                {/*<DialogItem name='Egor' id='3'/>*/}
+                {/*<DialogItem name='Pavel' id='4'/>*/}
 
             </div>
             <div className={s.messages}>
-                <Message message='Hi'/>
-                <Message message='How are you?'/>
-                <Message message='Im okay. Thanks'/>
+
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message={messagesData[2].message}/>
+
+
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,22 +3,27 @@ import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let postData = [
+        {id:1, message: 'Hi, how are you?', likesCount: 12},
+        {id:2, message: 'It\'s my first post. Ohoooo', likesCount: 30},
+        {id:3, message: 'Keep Calm guys!', likesCount: -999},
+    ]
     return (
-    <div className={s.postsBlock}>
-        <h2>My posts</h2>
-        <div>
-            <textarea>Text...</textarea>
+        <div className={s.postsBlock}>
+            <h2>My posts</h2>
+            <div>
+                <textarea>Text...</textarea>
+            </div>
+            <div>
+                <button>New Post</button>
+            </div>
+            <div className={s.posts}>
+                <Post message={postData[0].message} likes={postData[0].likesCount}/>
+                <Post message={postData[1].message} likes={postData[1].likesCount}/>
+                <Post message={postData[2].message} likes={postData[2].likesCount}/>
+            </div>
         </div>
-        <div>
-            <button>New Post</button>
-        </div>
-        <div className={s.posts}>
-            <Post message="First port" likes={Math.round(Math.random() * 1000)}/>
-            <Post message="First port" likes={Math.round(Math.random() * 1000)}/>
-            <Post message="First port" likes={Math.round(Math.random() * 1000)}/>
-            <Post message="First port" likes={Math.round(Math.random() * 1000)}/>
-        </div>
-    </div>
     )
 }
 
