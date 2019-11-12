b79c41e4d6852e864a260fae3e689264fb1d079d
Denis Gorshkov <satrasmatrassatras@gmail.com>
2019-11-12 10:34:27 +0300

    Dialogs small refactoring
    And some css changes

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,16 +5,16 @@ import {NavLink} from "react-router-dom";
 const DialogItem = (props) => {
     return (
         <div className={s.dialog}>
-            <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
+            <NavLink activeClassName={s.active} to={`/dialogs/${props.id}`}>{props.name}</NavLink>
         </div>
     )
-}
+};
 
 const Message = (props) => {
     return (
         <div className={s.message}>{props.message}</div>
     )
-}
+};
 
 
 const Dialogs = (props) => {
@@ -39,10 +39,8 @@ const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
-                {
-                    dialogsData.map( dialogItem => <DialogItem name={dialogItem.name} id={dialogItem.id}/>)
-                }
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                {dialogsData.map( dialogItem => <DialogItem name={dialogItem.name} id={dialogItem.id}/>)}
+                {/*<DialogItem name={dialogsData[0].name} id={dialogsData[0].id} isActive="active"/>*/}
                 {/*<DialogItem name='Viktoria' id='2'/>*/}
                 {/*<DialogItem name='Egor' id='3'/>*/}
                 {/*<DialogItem name='Pavel' id='4'/>*/}
